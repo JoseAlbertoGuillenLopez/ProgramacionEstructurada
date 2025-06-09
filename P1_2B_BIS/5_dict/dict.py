a029143eda6ce8b60bf9a8f8d3e818c1491ead39
JoseGuillen <jose_3141240120@utd.edu.mx>
2025-06-09 16:54:41 -0600

    Delete P1_2B_BIS/5_dict directory

--- P1_2B_BIS/5_dict/dict.py
+++
@@ -1,6 +0,0 @@
-'''Es un tipo de datos que se utiliza para almacenar datos de diferente tipo de datos pero en
-lugar de tener como las listas indices numericos tiene alfanumericos. Es decir es algo parecido a los objetos
-
-Tambien se conoce como arreglo asosiativo u Objeto JSON
-
-El diccionario es una coleccion ordenada y modificable. No hay miembros duplicados'''
